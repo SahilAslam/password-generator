482f6ad70f31644a4e9f7553669faa774d8c023e
SahilAslam <sahilaslam14@gmail.com>
2024-04-17 12:51:38 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <section>
+      <div className="container">
+        password generator
+      </div>
+    </section>
+  );
+}
+
+export default App;
